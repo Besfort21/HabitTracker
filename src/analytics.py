87a50836786dec0954c947f4e69413c907e9f95c
Besfort21 <besfort-avdyli1@hotmail.com>
2022-09-21 15:58:50 +0200

    analytics module

---
+++ src/analytics.py
@@ -0,0 +1,40 @@
+from model import Habit,Streak
+from db import conn
+from typing import List
+
+
+
+c = conn.cursor()
+
+def samePeriodicity() -> List[Habit]:
+    c.execute("select * from habits ORDER BY periodicity DESC")
+    results = c.fetchall()
+    same = []
+    for result in results:
+        same.append(Habit(*result))
+    return same
+
+def maxStreakAll() -> List[Streak]:
+    c.execute("select * from streaks WHERE streaks = (SELECT MAX(streaks) from streaks)")
+    results = c.fetchall()
+    max = []
+    for result in results:
+        max.append(Streak(*result))
+    return max
+
+def maxStreakHabit(position) -> List[Streak]:
+    position -= 1
+    c.execute("select * from streaks WHERE position = :position and streaks = (SELECT MAX(streaks) from streaks)",{'position':position})
+    results = c.fetchall()
+    max = []
+    for result in results:
+        max.append(Streak(*result))
+    return max
+
+def mostStruggle() ->List[Habit]:
+    c.execute("select * from habits ORDER BY brokenhabits DESC")
+    results = c.fetchall()
+    struggles = []
+    for result in results:
+        struggles.append(Habit(*result))
+    return struggles
